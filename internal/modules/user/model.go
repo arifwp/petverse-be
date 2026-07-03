b8ab7892c164449ba60bc6604c6a3c68c4814d38
Arif <arifwahyu696@gmail.com>
2026-07-03 14:07:27 +0700

    remove omitempty

--- internal/modules/user/model.go
+++ internal/modules/user/model.go
@@ -1,3 +1,4 @@
+// internal/modules/user/model.go
 package user
 
 import (
@@ -8,13 +9,13 @@ import (
 
 type User struct {
 	ID            uuid.UUID  `gorm:"type:uuid;primaryKey;column:id" json:"id"`
-	Name          *string    `gorm:"column:name" json:"name,omitempty"`
+	Name          *string    `gorm:"column:name" json:"name"`
 	Username      string     `gorm:"column:username;not null;uniqueIndex" json:"username"`
 	Email         string     `gorm:"column:email;not null;uniqueIndex" json:"email"`
-	AvatarURL     *string    `gorm:"column:avatar_url" json:"avatar_url,omitempty"`
+	AvatarURL     *string    `gorm:"column:avatar_url" json:"avatar_url"`
 	CreatedAt     time.Time  `gorm:"column:created_at;not null" json:"created_at"`
 	UpdatedAt     time.Time  `gorm:"column:updated_at;not null" json:"updated_at"`
-	LastLoginAt   *time.Time `gorm:"column:last_login_at" json:"last_login_at,omitempty"`
+	LastLoginAt   *time.Time `gorm:"column:last_login_at" json:"last_login_at"`
 	EmailVerified bool       `gorm:"column:email_verified;not null;default:false" json:"email_verified"`
 	PasswordHash  string     `gorm:"column:password;not null" json:"-"`
 }
